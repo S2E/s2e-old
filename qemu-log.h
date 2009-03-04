f721e3ac031f892af46f255a47d7f54a91317b30
The Android Open Source Project <initial-contribution@android.com>
2009-03-03 18:28:35 -0800

    auto import from //depot/cupcake/@135843

--- qemu-log.h
+++
@@ -1,7 +0,0 @@
-#ifndef QEMU_LOG_H
-#define QEMU_LOG_H
-
-extern FILE *logfile;
-extern int loglevel;
-
-#endif
--- qemu_socket.h
+++
@@ -1,8 +0,0 @@
-#ifndef _qemu_socket_h
-#define _qemu_socket_h
-
-#include "sockets.h"
-#define  socket_error()  socket_errno
-#define  closesocket     socket_close
-
-#endif /* _qemu_socket_h */
