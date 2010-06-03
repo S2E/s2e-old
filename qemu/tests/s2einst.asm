59de721b7d1820c28b2d2b1cc5efca318cd4dfc0
Vitaly Chipounov <vitaly.chipounov@epfl.ch>
2010-06-03 12:40:25 +0000

    Fork bomb in bios
    git-svn-id: https://dslabpc10.epfl.ch/svn/src/s2e/trunk@9969 f94e2f5b-dfd8-4992-a279-46519bc7c46e

--- qemu/tests/s2einst.asm
+++ qemu/tests/s2einst.asm
@@ -117,7 +117,7 @@ leave
 ret
 
 
-_s2e_print_expression:
+s2e_print_expression:
 push ebp
 mov ebp, esp
 
@@ -233,16 +233,51 @@ s2e_test:
 
     call s2e_enable
 
+    mov ecx, 0
+
+
+a:
+    push ecx
     call s2e_int
-    cmp eax, 1
-    jz lbl1
-    call s2e_kill_state
+    pop ecx
 
-    lbl1:
-    call s2e_kill_state
+    ;;Print the counter
+    push eax
+    push ecx
 
+    push 0
+    push ecx
+    call s2e_print_expression
+    add esp, 8
+
+    pop ecx
+    pop eax
+
+    ;;Print the symbolic value
+    push eax
+    push ecx
+
+    push 0
+    push eax
+    call s2e_print_expression
+    add esp, 8
+
+    pop ecx
+    pop eax
+
+    test eax, 1
+    jz exit1    ; if (i < symb) exit
+    inc ecx
+    jmp a
 
     call s2e_disable
+    call s2e_kill_state
+
+    exit1:
+    call s2e_disable
+    call s2e_kill_state
+
+
     hlt
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
