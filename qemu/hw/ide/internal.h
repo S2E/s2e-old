353a47ced35e70d438b0931d1f1aca1def51ee42
Volodymyr Kuznetsov <vova.kuznetsov@epfl.ch>
2010-06-25 12:45:12 +0000

    Avoid using int("asm $3") in the code
    git-svn-id: https://dslabpc10.epfl.ch/svn/src/s2e/trunk@10267 f94e2f5b-dfd8-4992-a279-46519bc7c46e

--- qemu/hw/ide/internal.h
+++ qemu/hw/ide/internal.h
@@ -497,10 +497,7 @@ static inline IDEState *idebus_active_if(IDEBus *bus)
 
 static inline IDEState *bmdma_active_if(BMDMAState *bmdma)
 {
-    //assert(bmdma->unit != (uint8_t)-1);
-    if (bmdma->unit == (uint8_t)-1) {
-        asm("int $3");
-    }
+    assert(bmdma->unit != (uint8_t)-1);
     return bmdma->bus->ifs + bmdma->unit;
 }
 
