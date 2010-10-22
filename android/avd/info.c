aosp-mirror/platform_external_qemu

Load state snapshot rather than booting to start emulator.
This patch adds support for an optional file "snapshots.img" in the
data directory of an AVD. This file should be an image formatted with
the qcow2 file system and will be mounted on -hdb when the emulator
starts up. If present, the emulator will attempt to load the snapshot
named "default-boot" from this image, rather than going through the
full boot procedure.

To control the behaviour of this functionality, this patch introduces
the following new command line options for the emulator:

  -snapshots <file>: override the default location of the
     snapshots file.
  -no-snapshots: do not load the snapshots file, even if an explicit
     path is given.
  -load-snapshot <name>: instead of loading "default-boot", load the
     state snapshot with the given name.
  -full-boot: force a full boot sequence, even if a snapshots file is
     mounted. Useful for creating snapshots.

Note that this functionality is experimental and will be enabled
only if the constant CONFIG_ANDROID_SNAPSHOTS in
android/config/config.h is set to "1" before building. It is turned
off by default.

Change-Id: Iee2e9096a27f3414bacfc271f90ef93a98730c82

android/avd/info.c
@@ -10,6 +10,7 @@
 ** GNU General Public License for more details.
 */
 #include "android/avd/info.h"
+#include "android/config/config.h"
 #include "android/utils/path.h"
 #include "android/utils/bufprint.h"
 #include "android/utils/filelock.h"
@@ -681,7 +682,30 @@ EXIT:
     return l->pPath[0];
 }
 
+/* Attempts to load an AVD image, but does not kill the process if loading
+ * fails.
+ */
+static void
+imageLoader_optional_load( ImageLoader *l, AvdImageType img_type,
+                           const char *forcedPath )
+{
+    imageLoader_set (l, img_type);
+    imageLoader_load(l, IMAGE_OPTIONAL |
+                        IMAGE_IGNORE_IF_LOCKED);
 
+    /* if the file was not found, ignore it */
+    if (l->pPath[0] && !path_exists(l->pPath[0]))
+    {
+        D("ignoring non-existing %s at %s: %s",
+          l->imageText, l->pPath[0], strerror(errno));
+
+        /* if the user provided the path by hand, warn him. */
+        if (forcedPath != NULL)
+            dwarning("ignoring non-existing %s image", l->imageText);
+
+        imageLoader_setPath(l, NULL);
+    }
+}
 
 /* find the correct path of all image files we're going to need
  * and lock the files that need it.
@@ -689,10 +713,13 @@ EXIT:
 static int
 _getImagePaths(AvdInfo*  i, AvdInfoParams*  params )
 {
-    int   wipeData  = (params->flags & AVDINFO_WIPE_DATA) != 0;
-    int   wipeCache = (params->flags & AVDINFO_WIPE_CACHE) != 0;
-    int   noCache   = (params->flags & AVDINFO_NO_CACHE) != 0;
-    int   noSdCard  = (params->flags & AVDINFO_NO_SDCARD) != 0;
+    int   wipeData    = (params->flags & AVDINFO_WIPE_DATA) != 0;
+    int   wipeCache   = (params->flags & AVDINFO_WIPE_CACHE) != 0;
+    int   noCache     = (params->flags & AVDINFO_NO_CACHE) != 0;
+    int   noSdCard    = (params->flags & AVDINFO_NO_SDCARD) != 0;
+#if CONFIG_ANDROID_SNAPSHOTS
+    int   noSnapshots = (params->flags & AVDINFO_NO_SNAPSHOTS) != 0;
+#endif
 
     ImageLoader  l[1];
 
@@ -778,24 +805,19 @@ _getImagePaths(AvdInfo*  i, AvdInfoParams*  params )
      * already used, we must ignore it.
      */
     if (!noSdCard) {
-        imageLoader_set (l, AVD_IMAGE_SDCARD);
-        imageLoader_load(l, IMAGE_OPTIONAL |
-                            IMAGE_IGNORE_IF_LOCKED);
-
-        /* if the file was not found, ignore it */
-        if (l->pPath[0] && !path_exists(l->pPath[0])) 
-        {
-            D("ignoring non-existing %s at %s: %s",
-              l->imageText, l->pPath[0], strerror(errno));
-
-            /* if the user provided the SD Card path by hand,
-             * warn him. */
-            if (params->forcePaths[AVD_IMAGE_SDCARD] != NULL)
-                dwarning("ignoring non-existing SD Card image");
-
-            imageLoader_setPath(l, NULL);
-        }
+        imageLoader_optional_load(l, AVD_IMAGE_SDCARD,
+                                  params->forcePaths[AVD_IMAGE_SDCARD]);
     }
+
+#if CONFIG_ANDROID_SNAPSHOTS
+    /* the state snapshot image. Mounting behaviour identical to
+     * SD card.
+     */
+    if (!noSnapshots) {
+        imageLoader_optional_load(l, AVD_IMAGE_SNAPSHOTS,
+                                  params->forcePaths[AVD_IMAGE_SNAPSHOTS]);
+    }
+#endif
 
     return 0;
 }
@@ -1146,9 +1168,12 @@ _getBuildConfigIni( AvdInfo*  i )
 static int
 _getBuildImagePaths( AvdInfo*  i, AvdInfoParams*  params )
 {
-    int   wipeData  = (params->flags & AVDINFO_WIPE_DATA) != 0;
-    int   noCache   = (params->flags & AVDINFO_NO_CACHE) != 0;
-    int   noSdCard  = (params->flags & AVDINFO_NO_SDCARD) != 0;
+    int   wipeData    = (params->flags & AVDINFO_WIPE_DATA) != 0;
+    int   noCache     = (params->flags & AVDINFO_NO_CACHE) != 0;
+    int   noSdCard    = (params->flags & AVDINFO_NO_SDCARD) != 0;
+#if CONFIG_ANDROID_SNAPSHOTS
+    int   noSnapshots = (params->flags & AVDINFO_NO_SNAPSHOTS) != 0;
+#endif
 
     char         temp[PATH_MAX], *p=temp, *end=p+sizeof temp;
     char*        srcData;
@@ -1262,6 +1287,15 @@ _getBuildImagePaths( AvdInfo*  i, AvdInfoParams*  params )
         imageLoader_set (l, AVD_IMAGE_SDCARD);
         imageLoader_load(l, IMAGE_OPTIONAL | IMAGE_IGNORE_IF_LOCKED);
     }
+
+#if CONFIG_ANDROID_SNAPSHOTS
+    /** State snapshots image
+     **/
+    if (!noSnapshots) {
+        imageLoader_set (l, AVD_IMAGE_SNAPSHOTS);
+        imageLoader_load(l, IMAGE_OPTIONAL | IMAGE_IGNORE_IF_LOCKED);
+    }
+#endif
 
     return 0;
 }

android/main.c
@@ -998,6 +998,16 @@ int main(int argc, char **argv)
                 D("autoconfig: -sdcard %s", opts->sdcard);
             }
         }
+
+#if CONFIG_ANDROID_SNAPSHOTS
+        if (!opts->snapshots && opts->datadir) {
+            bufprint(tmp, tmpend, "%s/snapshots.img", opts->datadir);
+            if (path_exists(tmp)) {
+                opts->snapshots = qemu_strdup(tmp);
+                D("autoconfig: -snapshots %s", opts->snapshots);
+            }
+        }
+#endif
     }
 
     /* setup the virtual device parameters from our options
@@ -1008,16 +1018,24 @@ int main(int argc, char **argv)
     if (opts->wipe_data) {
         android_avdParams->flags |= AVDINFO_WIPE_DATA | AVDINFO_WIPE_CACHE;
     }
+#if CONFIG_ANDROID_SNAPSHOTS
+    if (opts->no_snapshots) {
+        android_avdParams->flags |= AVDINFO_NO_SNAPSHOTS;
+    }
+#endif
 
     /* if certain options are set, we can force the path of
         * certain kernel/disk image files
         */
-    _forceAvdImagePath(AVD_IMAGE_KERNEL,     opts->kernel, "kernel", 1);
-    _forceAvdImagePath(AVD_IMAGE_INITSYSTEM, opts->system, "system", 1);
-    _forceAvdImagePath(AVD_IMAGE_RAMDISK,    opts->ramdisk,"ramdisk", 1);
-    _forceAvdImagePath(AVD_IMAGE_USERDATA,   opts->data,   "user data", 0);
-    _forceAvdImagePath(AVD_IMAGE_CACHE,      opts->cache,  "cache", 0);
-    _forceAvdImagePath(AVD_IMAGE_SDCARD,     opts->sdcard, "SD Card", 0);
+    _forceAvdImagePath(AVD_IMAGE_KERNEL,     opts->kernel,    "kernel", 1);
+    _forceAvdImagePath(AVD_IMAGE_INITSYSTEM, opts->system,    "system", 1);
+    _forceAvdImagePath(AVD_IMAGE_RAMDISK,    opts->ramdisk,   "ramdisk", 1);
+    _forceAvdImagePath(AVD_IMAGE_USERDATA,   opts->data,      "user data", 0);
+    _forceAvdImagePath(AVD_IMAGE_CACHE,      opts->cache,     "cache", 0);
+    _forceAvdImagePath(AVD_IMAGE_SDCARD,     opts->sdcard,    "SD Card", 0);
+#if CONFIG_ANDROID_SNAPSHOTS
+    _forceAvdImagePath(AVD_IMAGE_SNAPSHOTS,  opts->snapshots, "snapshots", 0);
+#endif
 
     /* we don't accept -skindir without -skin now
      * to simplify the autoconfig stuff with virtual devices
@@ -1375,6 +1393,35 @@ int main(int argc, char **argv)
             D("no SD Card image at '%s'", opts->sdcard);
         }
     }
+
+#if CONFIG_ANDROID_SNAPSHOTS
+    opts->snapshots = (char*) avdInfo_getImageFile(avd, AVD_IMAGE_SNAPSHOTS);
+    if (!opts->no_snapshots) {
+        if(opts->snapshots) {
+            if (path_exists(opts->snapshots)) {
+                args[n++] = "-hdb";
+                args[n++] = opts->snapshots;
+            } else {
+                D("no image at '%s', state snapshots disabled", opts->snapshots);
+            }
+        }
+
+        if (!opts->full_boot) {
+            args[n++] = "-loadvm";
+            if (opts->load_snapshot) {
+                args[n++] = opts->load_snapshot;
+            } else {
+                // name of state snapshot to load if not specified by user
+                args[n++] = "default-boot";
+            }
+        } else if (opts->load_snapshot) {
+            dwarning("option '-full-boot' overrides '-load-snapshot', continuing with boot sequence");
+        }
+    } else if (opts->load_snapshot || opts->snapshots) {
+        dwarning("option '-no-snapshots' overrides '-load-snapshot' and '-snapshots', "
+                 "continuing with full boot, state snapshots are disabled");
+    }
+#endif
 
     if (!opts->logcat || opts->logcat[0] == 0) {
         opts->logcat = getenv("ANDROID_LOG_TAGS");

S2E/s2e-old

  -snapstorage <file>: override the default location of the
     snapshot storage file.
  -no-snapstorage: do not load the snapshots file, even if an explicit
  -snapshot <name>: instead of loading "default-boot", load the state
     snapshot with the given name.
  -no-snapshot: do not load a snapshot, but force a full boot
     sequence, even if a snapshots file is mounted. Useful for
     creating snapshots.

android/avd/info.c
@@ -10,6 +10,7 @@
 ** GNU General Public License for more details.
 */
 #include "android/avd/info.h"
+#include "android/config/config.h"
 #include "android/utils/path.h"
 #include "android/utils/bufprint.h"
 #include "android/utils/filelock.h"
@@ -681,7 +682,30 @@ EXIT:
     return l->pPath[0];
 }
 
+/* Attempts to load an AVD image, but does not kill the process if loading
+ * fails.
+ */
+static void
+imageLoader_loadOptional( ImageLoader *l, AvdImageType img_type,
+                           const char *forcedPath )
+{
+    imageLoader_set (l, img_type);
+    imageLoader_load(l, IMAGE_OPTIONAL |
+                        IMAGE_IGNORE_IF_LOCKED);
 
+    /* if the file was not found, ignore it */
+    if (l->pPath[0] && !path_exists(l->pPath[0]))
+    {
+        D("ignoring non-existing %s at %s: %s",
+          l->imageText, l->pPath[0], strerror(errno));
+
+        /* if the user provided the path by hand, warn him. */
+        if (forcedPath != NULL)
+            dwarning("ignoring non-existing %s image", l->imageText);
+
+        imageLoader_setPath(l, NULL);
+    }
+}
 
 /* find the correct path of all image files we're going to need
  * and lock the files that need it.
@@ -689,10 +713,13 @@ EXIT:
 static int
 _getImagePaths(AvdInfo*  i, AvdInfoParams*  params )
 {
-    int   wipeData  = (params->flags & AVDINFO_WIPE_DATA) != 0;
-    int   wipeCache = (params->flags & AVDINFO_WIPE_CACHE) != 0;
-    int   noCache   = (params->flags & AVDINFO_NO_CACHE) != 0;
-    int   noSdCard  = (params->flags & AVDINFO_NO_SDCARD) != 0;
+    int   wipeData    = (params->flags & AVDINFO_WIPE_DATA) != 0;
+    int   wipeCache   = (params->flags & AVDINFO_WIPE_CACHE) != 0;
+    int   noCache     = (params->flags & AVDINFO_NO_CACHE) != 0;
+    int   noSdCard    = (params->flags & AVDINFO_NO_SDCARD) != 0;
+#if CONFIG_ANDROID_SNAPSHOTS
+    int   noSnapshots = (params->flags & AVDINFO_NO_SNAPSHOTS) != 0;
+#endif
 
     ImageLoader  l[1];
 
@@ -778,24 +805,19 @@ _getImagePaths(AvdInfo*  i, AvdInfoParams*  params )
      * already used, we must ignore it.
      */
     if (!noSdCard) {
-        imageLoader_set (l, AVD_IMAGE_SDCARD);
-        imageLoader_load(l, IMAGE_OPTIONAL |
-                            IMAGE_IGNORE_IF_LOCKED);
-
-        /* if the file was not found, ignore it */
-        if (l->pPath[0] && !path_exists(l->pPath[0])) 
-        {
-            D("ignoring non-existing %s at %s: %s",
-              l->imageText, l->pPath[0], strerror(errno));
-
-            /* if the user provided the SD Card path by hand,
-             * warn him. */
-            if (params->forcePaths[AVD_IMAGE_SDCARD] != NULL)
-                dwarning("ignoring non-existing SD Card image");
-
-            imageLoader_setPath(l, NULL);
-        }
+        imageLoader_loadOptional(l, AVD_IMAGE_SDCARD,
+                                 params->forcePaths[AVD_IMAGE_SDCARD]);
     }
+
+#if CONFIG_ANDROID_SNAPSHOTS
+    /* the state snapshot image. Mounting behaviour identical to
+     * SD card.
+     */
+    if (!noSnapshots) {
+        imageLoader_loadOptional(l, AVD_IMAGE_SNAPSHOTS,
+                                 params->forcePaths[AVD_IMAGE_SNAPSHOTS]);
+    }
+#endif
 
     return 0;
 }
@@ -1146,9 +1168,12 @@ _getBuildConfigIni( AvdInfo*  i )
 static int
 _getBuildImagePaths( AvdInfo*  i, AvdInfoParams*  params )
 {
-    int   wipeData  = (params->flags & AVDINFO_WIPE_DATA) != 0;
-    int   noCache   = (params->flags & AVDINFO_NO_CACHE) != 0;
-    int   noSdCard  = (params->flags & AVDINFO_NO_SDCARD) != 0;
+    int   wipeData    = (params->flags & AVDINFO_WIPE_DATA) != 0;
+    int   noCache     = (params->flags & AVDINFO_NO_CACHE) != 0;
+    int   noSdCard    = (params->flags & AVDINFO_NO_SDCARD) != 0;
+#if CONFIG_ANDROID_SNAPSHOTS
+    int   noSnapshots = (params->flags & AVDINFO_NO_SNAPSHOTS) != 0;
+#endif
 
     char         temp[PATH_MAX], *p=temp, *end=p+sizeof temp;
     char*        srcData;
@@ -1262,6 +1287,15 @@ _getBuildImagePaths( AvdInfo*  i, AvdInfoParams*  params )
         imageLoader_set (l, AVD_IMAGE_SDCARD);
         imageLoader_load(l, IMAGE_OPTIONAL | IMAGE_IGNORE_IF_LOCKED);
     }
+
+#if CONFIG_ANDROID_SNAPSHOTS
+    /** State snapshots image
+     **/
+    if (!noSnapshots) {
+        imageLoader_set (l, AVD_IMAGE_SNAPSHOTS);
+        imageLoader_load(l, IMAGE_OPTIONAL | IMAGE_IGNORE_IF_LOCKED);
+    }
+#endif
 
     return 0;
 }

android/main.c
@@ -998,6 +998,16 @@ int main(int argc, char **argv)
                 D("autoconfig: -sdcard %s", opts->sdcard);
             }
         }
+
+#if CONFIG_ANDROID_SNAPSHOTS
+        if (!opts->snapstorage && opts->datadir) {
+            bufprint(tmp, tmpend, "%s/snapshots.img", opts->datadir);
+            if (path_exists(tmp)) {
+                opts->snapstorage = qemu_strdup(tmp);
+                D("autoconfig: -snapstorage %s", opts->snapstorage);
+            }
+        }
+#endif
     }
 
     /* setup the virtual device parameters from our options
@@ -1008,16 +1018,24 @@ int main(int argc, char **argv)
     if (opts->wipe_data) {
         android_avdParams->flags |= AVDINFO_WIPE_DATA | AVDINFO_WIPE_CACHE;
     }
+#if CONFIG_ANDROID_SNAPSHOTS
+    if (opts->no_snapstorage) {
+        android_avdParams->flags |= AVDINFO_NO_SNAPSHOTS;
+    }
+#endif
 
     /* if certain options are set, we can force the path of
         * certain kernel/disk image files
         */
-    _forceAvdImagePath(AVD_IMAGE_KERNEL,     opts->kernel, "kernel", 1);
-    _forceAvdImagePath(AVD_IMAGE_INITSYSTEM, opts->system, "system", 1);
-    _forceAvdImagePath(AVD_IMAGE_RAMDISK,    opts->ramdisk,"ramdisk", 1);
-    _forceAvdImagePath(AVD_IMAGE_USERDATA,   opts->data,   "user data", 0);
-    _forceAvdImagePath(AVD_IMAGE_CACHE,      opts->cache,  "cache", 0);
-    _forceAvdImagePath(AVD_IMAGE_SDCARD,     opts->sdcard, "SD Card", 0);
+    _forceAvdImagePath(AVD_IMAGE_KERNEL,     opts->kernel,      "kernel", 1);
+    _forceAvdImagePath(AVD_IMAGE_INITSYSTEM, opts->system,      "system", 1);
+    _forceAvdImagePath(AVD_IMAGE_RAMDISK,    opts->ramdisk,     "ramdisk", 1);
+    _forceAvdImagePath(AVD_IMAGE_USERDATA,   opts->data,        "user data", 0);
+    _forceAvdImagePath(AVD_IMAGE_CACHE,      opts->cache,       "cache", 0);
+    _forceAvdImagePath(AVD_IMAGE_SDCARD,     opts->sdcard,      "SD Card", 0);
+#if CONFIG_ANDROID_SNAPSHOTS
+    _forceAvdImagePath(AVD_IMAGE_SNAPSHOTS,  opts->snapstorage, "snapshots", 0);
+#endif
 
     /* we don't accept -skindir without -skin now
      * to simplify the autoconfig stuff with virtual devices
@@ -1375,6 +1393,37 @@ int main(int argc, char **argv)
             D("no SD Card image at '%s'", opts->sdcard);
         }
     }
+
+#if CONFIG_ANDROID_SNAPSHOTS
+    if (!opts->no_snapstorage) {
+        opts->snapstorage = (char*) avdInfo_getImageFile(avd, AVD_IMAGE_SNAPSHOTS);
+        if(opts->snapstorage) {
+            if (path_exists(opts->snapstorage)) {
+                args[n++] = "-hdb";
+                args[n++] = opts->snapstorage;
+            } else {
+                D("no image at '%s', state snapshots disabled", opts->snapstorage);
+            }
+        }
+
+        if (!opts->no_snapshot) {
+            args[n++] = "-loadvm";
+            if (opts->snapshot) {
+                args[n++] = opts->snapshot;
+            } else {
+                // name of state snapshot to load if not specified by user
+                args[n++] = "default-boot";
+            }
+        } else if (opts->snapshot) {
+            dwarning("option '-no-snapshot' overrides '-snapshot', continuing with boot sequence");
+        }
+    } else if (opts->snapshot || opts->snapstorage) {
+        dwarning("option '-no-snapstorage' overrides '-snapshot' and '-snapstorage', "
+                 "continuing with full boot, state snapshots are disabled");
+    } else if (opts->no_snapshot) {
+        D("ignoring redundant option '-no-snapshot' implied by '-no-snapstorage'");
+    }
+#endif
 
     if (!opts->logcat || opts->logcat[0] == 0) {
         opts->logcat = getenv("ANDROID_LOG_TAGS");