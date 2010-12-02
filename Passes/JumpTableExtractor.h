da132b98b25973e7ff6fe69e36d6074b91b09ff5
Vitaly Chipounov <vitaly.chipounov@epfl.ch>
2010-12-02 17:27:40 +0100

    Extracting offsets of jump tables

---
+++ Passes/JumpTableExtractor.h
@@ -0,0 +1,38 @@
+#ifndef _INSTR_JMPTBL_EXTRACTOR_PASS_H_
+
+#define _INSTR_JMPTBL_EXTRACTOR_PASS_H_
+
+#include "llvm/Pass.h"
+#include "llvm/Function.h"
+#include "llvm/Instructions.h"
+
+#include <set>
+
+/**
+ * Extracts all constant integers from an LLVM function.
+ */
+struct JumpTableExtractor : public llvm::FunctionPass {
+  static char ID;
+  JumpTableExtractor() : FunctionPass((intptr_t)&ID) {
+    m_jumpTableAddress = 0;
+    m_jumpTableSize = 0;
+  }
+
+private:
+  uint64_t m_jumpTableAddress;
+  uint64_t m_jumpTableSize;
+
+  llvm::Value *getIndirectCallAddress(llvm::Function &F);
+  uint64_t getOffset(llvm::Value *address) const;
+
+public:
+  virtual bool runOnFunction(llvm::Function &F);
+
+  uint64_t getJumpTableAddress() const {
+      return m_jumpTableAddress;
+  }
+
+};
+
+
+#endif
