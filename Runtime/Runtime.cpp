166d89cee5a95a587198d7d7ffdf9b6f203c5478
Vitaly Chipounov <vitaly.chipounov@epfl.ch>
2010-12-24 18:07:15 +0100

    Some cleanup + comments

--- Runtime/Runtime.cpp
+++ Runtime/Runtime.cpp
@@ -18,16 +18,34 @@ extern "C" {
 #define DBGPRINT(x)
 #endif
 
+//Type of a translated function, as produced by the
+//static translator
 typedef void (*x2l_entry_point)(uint64_t *opaque);
 
+/////////////////////////////////////////////////////
+//RUNTIME CALL RESOLUTION
+//The runtime intercepts all calls to native program counters,
+//translates these program counters to the addresses of
+//translated LLVM functions, and invokes the translated function.
+
+//List of pointers to translated LLVM functions
 extern x2l_entry_point __x2l_g_functionlist_ptr[];
+
+//List of "pointers" to native functions, as defined
+//by the original binary
 extern uint64_t __x2l_g_functionlist_val[];
+
+//Number of functions in the binary
 extern unsigned __x2l_g_functionCount;
 
-FILE *libc____sF = stdout;
-
+//Mapping from a native program counter to the corresponding
+//translated LLVM function
 typedef std::map<uint64_t, x2l_entry_point> functionmap_t;
 static functionmap_t s_functions;
+
+/////////////////////////////////////////////////////
+
+
 
 #define STACK_SIZE  0x10000
 #define STATE_SIZE 0x11000
@@ -51,18 +69,20 @@ static void initialize_function_map(void)
     }
 }
 
-void instruction_marker(uint64_t pc)
-{
-    DBGPRINT((stdout, "PC: 0x%llx\n", pc));
-}
 
-void *__x2e_findFunction(uint32_t target)
+
+static void *x2e_findFunction(uint32_t target)
 {
     functionmap_t::const_iterator it = s_functions.find(target);
     if (it == s_functions.end()) {
         return NULL;
     }
     return (void*)(*it).second;
+}
+
+void instruction_marker(uint64_t pc)
+{
+    DBGPRINT((stdout, "PC: 0x%llx\n", pc));
 }
 
 void call_marker(uint64_t target, int isInlinable, uint64_t *opaque)
@@ -99,6 +119,11 @@ void __stl_mmu(target_ulong addr, uint32_t val, int mmu_idx)
     *(uint32_t*)addr = val;
 }
 
+
+/////////////////////////////////////////////////////
+//PTHREAD threading support
+/////////////////////////////////////////////////////
+
 typedef  void* (*thread_routine_t)(void *);
 typedef  void (*transalted_thread_routine_t)(uint64_t *);
 
@@ -107,11 +132,17 @@ typedef struct {
     transalted_thread_routine_t routine;
 }x2l_thread_t;
 
+//This function wraps the original thread routine.
+//It sets up the execution environment for the original thread.
 static void *__x2e_thread_routine(void *opaque)
 {
     x2l_thread_t *threadParameters = (x2l_thread_t*)opaque;
     uint32_t *stack = new uint32_t[STACK_SIZE];
-    CPUState *penv = new CPUState;
+
+    //XXX: Ugly hack!
+    //Since we compile this module in 32-bit, the compiler thinks the CPUState is smaller...
+    //So we allocate it manually, to make it big enough
+    CPUState *penv = (CPUState*)malloc(STATE_SIZE);
 
     stack[STACK_SIZE/sizeof(uint32) - 4] = (uint32_t)threadParameters->parameter;
     stack[STACK_SIZE/sizeof(uint32) - 5] = 0xDEADBEEF; //dummy return address
@@ -133,19 +164,23 @@ int __attribute__((noinline)) libc__pthread_create(pthread_t  *  thread, pthread
 
     fprintf(stdout, "pthread_create %p %p %p %p\n", thread, attr, start_routine, arg);
     transalted_thread_routine_t x2l_routine =
-            (transalted_thread_routine_t)__x2e_findFunction((uint64_t)start_routine);
+            (transalted_thread_routine_t)x2e_findFunction((uint64_t)start_routine);
+
+    //XXX: what if the code gives us the pointer to a valid LLVM function
+    //(e.g., after relocation/patching...)
 
     if (!x2l_routine) {
+        //The translated code is trying to call arbitrary code...
         fprintf(stdout, "Could not find thread routine %p\n", start_routine);
         exit(-1);
     }
 
-    //Create the stack here
+    //Allocate space for the execution environment
     x2l_thread_t *threadDesc = new x2l_thread_t;
     threadDesc->parameter = arg;
     threadDesc->routine = x2l_routine;
 
-
+    //Create the thread
     int ret = pthread_create(thread, attr, __x2e_thread_routine, threadDesc);
     if (ret) {
         fprintf(stdout, "pthread_create failed: %d\n", ret);
@@ -156,17 +191,16 @@ int __attribute__((noinline)) libc__pthread_create(pthread_t  *  thread, pthread
 
 
 extern "C" {
-    int __attribute__((noinline))  libc__fputs(const char * a, FILE * b);
+
+//Defined in the bitcode library.
+//XXX: We should move these things to the runtime?
+int __attribute__((noinline))  libc__fputs(const char * a, FILE * b);
 
 int main(int argc, char **argv)
 {
     CPUState *penv;
 
     initialize_function_map();
-
-    libc__fputs("test\n", stdout);
-    fprintf(stdout, "stdout=%x\n", stdout);
-    fflush(stdout);
 
     //XXX: Ugly hack!
     //Since we compile this module in 32-bit, the compiler thinks the CPUState is smaller...
