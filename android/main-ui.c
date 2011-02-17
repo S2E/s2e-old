6b8555cac04862908bf6c64565061f2c8e871ad7
David 'Digit' Turner <digit@android.com>
2011-02-17 04:28:19 +0100

    Fix "avd name" bug.
    The console's 'avd name' command returned "unknown" incorrectly.
    This was due to a missing option when invoking the core, even in
    the standalone case.
    
    Fixes bug 3454261
    
    Change-Id: I7efcfc50792231f57dc76d3c11e7968faf057e12
    NOTE: This is a quick fix for tools_r10. A proper fix should instead
          move the AVD name to auto-generated qemu-hardware.ini instead of
          using a core command-line option.

--- android/main-ui.c
+++ android/main-ui.c
@@ -1432,6 +1432,9 @@ int main(int argc, char **argv)
     args[n++] = "unix";
 #endif
 
+    args[n++] = "-android-avdname";
+    args[n++] = avdInfo_getName(avd);
+
     /* Set up the interfaces for inter-emulator networking */
     if (opts->shared_net_id) {
         unsigned int shared_net_id = atoi(opts->shared_net_id);
