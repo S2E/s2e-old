821a9c504ab9ffb59d0047412fcc5d64e87bf40e
Vitaly Chipounov <vitaly.chipounov@epfl.ch>
2011-07-11 21:42:47 +0200

    More tests

--- qemu/roms/s2ebios/s2e-test-statemanager.asm
+++ qemu/roms/s2ebios/s2e-test-statemanager.asm
@@ -2,10 +2,12 @@
 
 msg_sm: db "FAILED - Must have only one state after the barrier", 0
 msg_sminst: db "FAILED - Must have only one instance after the barrier", 0
-msg_ok: db "SUCCESS", 0
 
 s2e_sm_test:
-    call s2e_sm_fork
+
+    push 4
+    call s2e_fork_depth
+    add esp, 4
 
     call s2e_sm_succeed
 
@@ -46,23 +48,5 @@ sst2:
     ret
 
 
-;Fork lots of states
-s2e_sm_fork:
-    push ebp
-    mov ebp, esp
-    sub esp, 4
 
-    call s2e_fork_enable
-
-    mov dword [ebp - 4], 4  ; Set forking depth to 4 (2^^4 states)
-ssf1:
-    call s2e_int
-    cmp eax, 0
-    ja ssf2
-ssf2:
-    dec dword [ebp - 4]; Decrement forking depth
-    jnz ssf1
-
-    leave
-    ret
 
