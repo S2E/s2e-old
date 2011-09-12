e3b0c763c9457dfaebba350f517b13115e46f731
Vitaly Chipounov <vitaly.chipounov@epfl.ch>
2011-09-12 16:47:17 +0200

    s2ecmd utility to invoke custom instructions from command line

---
+++ guest/s2ecmd/s2ecmd.c
@@ -0,0 +1,120 @@
+/*
+ * S2E Selective Symbolic Execution Framework
+ *
+ * Copyright (c) 2010, Dependable Systems Laboratory, EPFL
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Dependable Systems Laboratory, EPFL nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE DEPENDABLE SYSTEMS LABORATORY, EPFL BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Currently maintained by:
+ *    Vitaly Chipounov <vitaly.chipounov@epfl.ch>
+ *    Volodymyr Kuznetsov <vova.kuznetsov@epfl.ch>
+ *
+ * All contributors are listed in S2E-AUTHORS file.
+ *
+ */
+
+#include <s2e.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef void (*cmd_handler_t)(const char **args);
+
+typedef struct _cmd_t {
+    char *name;
+    cmd_handler_t handler;
+    unsigned args_count;
+}cmd_t;
+
+void handler_kill(const char **args)
+{
+    int status = atoi(args[0]);
+    const char *message = args[1];
+    s2e_kill_state(status, message);
+}
+
+void handler_message(const char **args)
+{
+    s2e_message(args[0]);
+}
+
+
+#define COMMAND(c, args) {#c, handler_##c, args}
+
+static cmd_t s_commands[] = {
+    COMMAND(kill, 2),
+    COMMAND(message, 1),
+    {NULL, NULL, 0}
+};
+
+void print_commands()
+{
+    unsigned i=0;
+    printf("%-15s  %s\n\n", "Command name", "Argument count");
+    while(s_commands[i].handler) {
+        printf("%-15s  %d\n", s_commands[i].name, s_commands[i].args_count);
+        ++i;
+    }
+}
+
+int find_command(const char *cmd)
+{
+    unsigned i=0;
+    while(s_commands[i].handler) {
+        if (!strcmp(s_commands[i].name, cmd)) {
+            return i;
+        }
+        ++i;
+    }
+    return -1;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2) {
+        print_commands();
+        return -1;
+    }
+
+    const char *cmd = argv[1];
+    int cmd_index = find_command(cmd);
+
+    if (cmd_index == -1) {
+        printf("Command %s not found\n", cmd);
+        return -1;
+    }
+
+    argc -= 2;
+    ++argv;
+    ++argv;
+
+    if (argc != s_commands[cmd_index].args_count) {
+        printf("Invalid number of arguments supplied (%d instead of %d)\n", argc, s_commands[cmd_index].args_count);
+        return -1;
+    }
+
+    s_commands[cmd_index].handler((const char**)argv);
+
+    return 0;
+}
