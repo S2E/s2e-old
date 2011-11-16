6755169e03c45d174dad185b8d58989d88e6ec02
Vitaly Chipounov <vitaly.chipounov@epfl.ch>
2011-11-16 18:15:35 +0100

    Imported intrinsics cleaner from Cloud9

--- klee/lib/Module/IntrinsicCleaner.cpp
+++ klee/lib/Module/IntrinsicCleaner.cpp
@@ -9,6 +9,7 @@
 
 #include "Passes.h"
 
+#include "klee/Config/config.h"
 #include "llvm/Constants.h"
 #include "llvm/DerivedTypes.h"
 #include "llvm/Function.h"
@@ -16,17 +17,16 @@
 #include "llvm/Instruction.h"
 #include "llvm/Instructions.h"
 #include "llvm/IntrinsicInst.h"
-#if !(LLVM_VERSION_MAJOR == 2 && LLVM_VERSION_MINOR < 7)
-#include "llvm/Support/ErrorHandling.h"
 #include "llvm/LLVMContext.h"
-#endif
 #include "llvm/Module.h"
 #include "llvm/Pass.h"
 #include "llvm/Type.h"
+#include "llvm/Support/IRBuilder.h"
+#include "llvm/Support/CallSite.h"
 #include "llvm/Transforms/Scalar.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
 #include "llvm/Target/TargetData.h"
-#include "llvm/Support/IRBuilder.h"
+#include "llvm/Support/ErrorHandling.h"
 
 using namespace llvm;
 
@@ -129,24 +129,101 @@ static Value *LowerBSWAP(LLVMContext &Context, Value *V, Instruction *IP) {
   return V;
 }
 
+
 char IntrinsicCleanerPass::ID;
+
+/// ReplaceCallWith - This function is used when we want to lower an intrinsic
+/// call to a call of an external function.  This handles hard cases such as
+/// when there was already a prototype for the external function, and if that
+/// prototype doesn't match the arguments we expect to pass in.
+template <class ArgIt>
+static CallInst *ReplaceCallWith(const char *NewFn, CallInst *CI,
+                                 ArgIt ArgBegin, ArgIt ArgEnd,
+                                 const Type *RetTy) {
+  // If we haven't already looked up this function, check to see if the
+  // program already contains a function with this name.
+  Module *M = CI->getParent()->getParent()->getParent();
+  // Get or insert the definition now.
+  std::vector<const Type *> ParamTys;
+  for (ArgIt I = ArgBegin; I != ArgEnd; ++I)
+    ParamTys.push_back((*I)->getType());
+  Constant* FCache = M->getOrInsertFunction(NewFn,
+                                  FunctionType::get(RetTy, ParamTys, false));
+
+  IRBuilder<> Builder(CI->getParent(), CI);
+  SmallVector<Value *, 8> Args(ArgBegin, ArgEnd);
+  CallInst *NewCI = Builder.CreateCall(FCache, Args.begin(), Args.end());
+  NewCI->setName(CI->getName());
+  if (!CI->use_empty())
+    CI->replaceAllUsesWith(NewCI);
+  CI->eraseFromParent();
+  return NewCI;
+}
+
+static void ReplaceFPIntrinsicWithCall(CallInst *CI, const char *Fname,
+                                       const char *Dname,
+                                       const char *LDname) {
+  CallSite CS(CI);
+  switch (CI->getArgOperand(0)->getType()->getTypeID()) {
+  default: assert(false && "Invalid type in intrinsic");
+  case Type::FloatTyID:
+    ReplaceCallWith(Fname, CI, CS.arg_begin(), CS.arg_end(),
+                  Type::getFloatTy(CI->getContext()));
+    break;
+  case Type::DoubleTyID:
+    ReplaceCallWith(Dname, CI, CS.arg_begin(), CS.arg_end(),
+                  Type::getDoubleTy(CI->getContext()));
+    break;
+  case Type::X86_FP80TyID:
+  case Type::FP128TyID:
+  case Type::PPC_FP128TyID:
+    ReplaceCallWith(LDname, CI, CS.arg_begin(), CS.arg_end(),
+                  CI->getArgOperand(0)->getType());
+    break;
+  }
+}
+
+static void ReplaceIntIntrinsicWithCall(CallInst *CI, const char *Fname,
+                                       const char *Dname,
+                                       const char *LDname) {
+  CallSite CS(CI);
+  switch (CI->getArgOperand(0)->getType()->getTypeID()) {
+  default: assert(false && "Invalid type in intrinsic");
+  case Type::IntegerTyID:
+		const IntegerType* itype = (const IntegerType*)CI->getArgOperand(0)->getType();
+		const IntegerType* btype = IntegerType::get(CI->getContext(), 1);
+		switch(itype->getBitWidth()) {
+			case 16:
+    		ReplaceCallWith(Fname, CI, CS.arg_begin(), CS.arg_end(),
+                  StructType::get(CI->getContext(), itype, btype, NULL));
+    		break;
+  		case 32:
+    		ReplaceCallWith(Dname, CI, CS.arg_begin(), CS.arg_end(),
+                  StructType::get(CI->getContext(), itype, btype, NULL));
+    		break;
+  		case 64:
+    		ReplaceCallWith(LDname, CI, CS.arg_begin(), CS.arg_end(),
+                  StructType::get(CI->getContext(), itype, btype, NULL));
+    		break;
+		}
+		break;
+  }
+}
 
 bool IntrinsicCleanerPass::runOnModule(Module &M) {
   bool dirty = false;
   for (Module::iterator f = M.begin(), fe = M.end(); f != fe; ++f)
-    for (Function::iterator b = f->begin(), be = f->end(); b != be; ++b)
-      dirty |= runOnBasicBlock(*b);
+    for (Function::iterator b = f->begin(), be = f->end(); b != be;)
+      dirty |= runOnBasicBlock(*(b++));
   return dirty;
 }
 
 bool IntrinsicCleanerPass::runOnFunction(Function &F) {
   bool dirty = false;
-
-    for (Function::iterator b = F.begin(), be = F.end(); b != be; ++b)
-      dirty |= runOnBasicBlock(*b);
+    for (Function::iterator b = F.begin(), be = F.end(); b != be;)
+      dirty |= runOnBasicBlock(*(b++));
   return dirty;
 }
-
 
 bool IntrinsicCleanerPass::runOnBasicBlock(BasicBlock &b) { 
   bool dirty = false;
@@ -157,6 +234,7 @@ bool IntrinsicCleanerPass::runOnBasicBlock(BasicBlock &b) {
     // increment now since LowerIntrinsic deletion makes iterator invalid.
     ++i;  
     if(ii) {
+			CallSite CS(ii);
       switch (ii->getIntrinsicID()) {
       case Intrinsic::vastart:
       case Intrinsic::vaend:
@@ -168,8 +246,8 @@ bool IntrinsicCleanerPass::runOnBasicBlock(BasicBlock &b) {
         // FIXME: This is much more target dependent than just the word size,
         // however this works for x86-32 and x86-64.
       case Intrinsic::vacopy: { // (dst, src) -> *((i8**) dst) = *((i8**) src)
-        Value *dst = ii->getOperand(1);
-        Value *src = ii->getOperand(2);
+        Value *dst = ii->getArgOperand(0);
+        Value *src = ii->getArgOperand(1);
 
         if (WordSize == 4) {
           Type *i8pp = PointerType::getUnqual(PointerType::getUnqual(Type::getInt8Ty(getGlobalContext())));
@@ -196,40 +274,7 @@ bool IntrinsicCleanerPass::runOnBasicBlock(BasicBlock &b) {
         break;
       }
 
-#if (LLVM_VERSION_MAJOR == 2 && LLVM_VERSION_MINOR < 7)
-      case Intrinsic::dbg_stoppoint: {
-        // We can remove this stoppoint if the next instruction is
-        // sure to be another stoppoint. This is nice for cleanliness
-        // but also important for switch statements where it can allow
-        // the targets to be joined.
-        bool erase = false;
-        if (isa<DbgStopPointInst>(i) ||
-            isa<UnreachableInst>(i)) {
-          erase = true;
-        } else if (isa<BranchInst>(i) ||
-                   isa<SwitchInst>(i)) {
-          BasicBlock *bb = i->getParent();
-          erase = true;
-          for (succ_iterator it=succ_begin(bb), ie=succ_end(bb);
-               it!=ie; ++it) {
-            if (!isa<DbgStopPointInst>(it->getFirstNonPHI())) {
-              erase = false;
-              break;
-            }
-          }
-        }
-
-        if (erase) {
-          ii->eraseFromParent();
-          dirty = true;
-        }
-        break;
-      }
-
-      case Intrinsic::dbg_region_start:
-      case Intrinsic::dbg_region_end:
-      case Intrinsic::dbg_func_start:
-#endif
+      case Intrinsic::dbg_value:
       case Intrinsic::dbg_declare:
         // Remove these regardless of lower intrinsics flag. This can
         // be removed once IntrinsicLowering is fixed to not have bad
@@ -237,12 +282,81 @@ bool IntrinsicCleanerPass::runOnBasicBlock(BasicBlock &b) {
         ii->eraseFromParent();
         dirty = true;
         break;
-
-      case Intrinsic::bswap:
-        ii->replaceAllUsesWith(LowerBSWAP(getGlobalContext(), ii->getOperand(1), ii));
-        ii->eraseFromParent();
+      case Intrinsic::memory_barrier:
+      case Intrinsic::atomic_swap:
+      case Intrinsic::atomic_cmp_swap:
+      case Intrinsic::atomic_load_add:
+      case Intrinsic::atomic_load_sub:
+        break;      
+  		case Intrinsic::powi:
+    		ReplaceFPIntrinsicWithCall(ii, "powif", "powi", "powil");
+				dirty = true;
+    		break;
+  		case Intrinsic::uadd_with_overflow:
+				ReplaceIntIntrinsicWithCall(ii, "uadds", "uadd", "uaddl");
+				dirty = true;
+    		break;
+  		case Intrinsic::trap:
+    		// Link with abort
+        ReplaceCallWith("abort", ii, CS.arg_end(), CS.arg_end(),
+                        Type::getVoidTy(getGlobalContext()));
+				dirty = true;
         break;
-                    
+      case Intrinsic::memset:
+      case Intrinsic::memcpy:
+      case Intrinsic::memmove: {
+        LLVMContext &Ctx = ii->getContext();
+
+        Value *dst = ii->getArgOperand(0);
+        Value *src = ii->getArgOperand(1);
+        Value *len = ii->getArgOperand(2);
+
+        BasicBlock *BB = ii->getParent();
+        Function *F = BB->getParent();
+
+        BasicBlock *exitBB = BB->splitBasicBlock(ii);
+        BasicBlock *headerBB = BasicBlock::Create(Ctx, Twine(), F, exitBB);
+        BasicBlock *bodyBB  = BasicBlock::Create(Ctx, Twine(), F, exitBB);
+
+        // Enter the loop header
+        BB->getTerminator()->eraseFromParent();
+        BranchInst::Create(headerBB, BB);
+
+        // Create loop index
+        PHINode *idx = PHINode::Create(len->getType(), Twine(), headerBB);
+        idx->addIncoming(ConstantInt::get(len->getType(), 0), BB);
+
+        // Check loop condition, then move to the loop body or exit the loop
+        Value *loopCond = ICmpInst::Create(Instruction::ICmp, ICmpInst::ICMP_ULT,
+                                           idx, len, Twine(), headerBB);
+        BranchInst::Create(bodyBB, exitBB, loopCond, headerBB);
+
+        // Get value to store
+        Value *val;
+        if (ii->getIntrinsicID() == Intrinsic::memset) {
+          val = src;
+        } else {
+          Value *srcPtr = GetElementPtrInst::Create(src, idx, Twine(), bodyBB);
+          val = new LoadInst(srcPtr, Twine(), bodyBB);
+        }
+
+        // Store the value
+        Value* dstPtr = GetElementPtrInst::Create(dst, idx, Twine(), bodyBB);
+        new StoreInst(val, dstPtr, bodyBB);
+
+        // Update index and branch back
+        Value* newIdx = BinaryOperator::Create(Instruction::Add,
+                    idx, ConstantInt::get(len->getType(), 1), Twine(), bodyBB);
+        BranchInst::Create(headerBB, bodyBB);
+        idx->addIncoming(newIdx, bodyBB);
+
+        ii->eraseFromParent();
+
+        // Update iterators to continue in the next BB
+        i = exitBB->begin();
+        ie = exitBB->end();
+        break;
+      }
       default:
         if (LowerIntrinsics)
           IL->LowerIntrinsicCall(ii);
@@ -288,5 +402,6 @@ bool IntrinsicFunctionCleanerPass::runOnBasicBlock(llvm::BasicBlock &b)
 
   return dirty;
 }
+
 
 }
