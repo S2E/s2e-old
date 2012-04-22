b25bc1abe9d56dda7fdd8496adc1a16ac417e687
Vitaly Chipounov <vitaly.chipounov@epfl.ch>
2012-04-23 00:26:18 +0200

    Ditched previous add with overflow implementations. They are now fully implemented in the intrinsic cleaner pass.

--- qemu/llvm-lib.h
+++ qemu/llvm-lib.h
@@ -99,52 +99,5 @@ void trace_port(char *buf, const char *prefix, uint32_t port, uint32_t pc)
     *buf = 0;
 }
 
-//This is needed to replace some LLVM intrinsic functions.
-#define max(a,b) ((a>b) ? a : b)
-
-typedef struct{
-        u_int16_t v;
-        bool c;
-}u_int16_carry;
-
-u_int16_carry uadds(u_int16_t a, u_int16_t b);
-
-u_int16_carry uadds(u_int16_t a, u_int16_t b)
-{
-        u_int16_carry res;
-        res.v = a+b;
-        res.c = (res.v < max(a,b));
-        return res;
-}
-
-typedef struct{
-        u_int32_t v;
-        bool c;
-}u_int32_carry;
-
-u_int32_carry uadd(u_int32_t a, u_int32_t b);
-
-u_int32_carry uadd(u_int32_t a, u_int32_t b)
-{
-        u_int32_carry res;
-        res.v = a+b;
-        res.c = (res.v < max(a,b));
-        return res;
-}
-
-typedef struct{
-        u_int64_t v;
-        bool c;
-}u_int64_carry;
-
-u_int64_carry uaddl(u_int64_t a, u_int64_t b);
-
-u_int64_carry uaddl(u_int64_t a, u_int64_t b)
-{
-        u_int64_carry res;
-        res.v = a+b;
-        res.c = (res.v < max(a,b));
-        return res;
-}
 
 #endif
