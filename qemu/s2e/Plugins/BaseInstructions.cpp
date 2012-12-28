a215002704c4fa0d92c463d85f2c61d698aa077d
Vitaly Chipounov <vitaly.chipounov@epfl.ch>
2012-12-28 19:47:45 +0100

    Compilation fix
    Signed-off-by: Vitaly Chipounov <vitaly.chipounov@epfl.ch>

--- qemu/s2e/Plugins/BaseInstructions.cpp
+++ qemu/s2e/Plugins/BaseInstructions.cpp
@@ -400,7 +400,7 @@ void BaseInstructions::assume(S2EExecutionState *state)
     //the existing path constraints
     bool isValid = true;
     if (ConcolicMode) {
-        ConstantExpr *ce = dyn_cast<ConstantExpr>(state->concolics->evaluate(boolExpr));
+        ConstantExpr *ce = dyn_cast<ConstantExpr>(state->concolics.evaluate(boolExpr));
         assert(ce && "Expression must be constant here");
         if (!ce->isTrue()) {
             isValid = false;
