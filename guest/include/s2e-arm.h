924dd64a81eb8e7b325b862551a6dfea0ebefac5
Luca Bruno <lucab@debian.org>
2013-11-14 10:55:47 +0100

    Make guest tools more arch independent
    Signed-off-by: Luca Bruno <lucab@debian.org>

--- guest/include/s2e-arm.h
+++ guest/include/s2e-arm.h
@@ -33,41 +33,23 @@
  *
  */
 
+// custom S2E-instructions for native ARM programs
 
-/******************************************************************************
- * s2earm.h
- * custom S2E-instructions for native ARM programs
- *
- ******************************************************************************
+#include <assert.h>
 
-/** Forces the read of every byte of the specified string.
-  * This makes sure the memory pages occupied by the string are paged in
-  * before passing them to S2E, which can't page in memory by itself. */
-static inline void __s2e_touch_string(volatile const char *string)
-{
-    while(*string) {
-        ++string;
-    }
+#define S2E_INSTRUCTION_COMPLEX(val1, val2)             \
+        ".WORD 0xFF" #val1 #val2 "00\n\t"
 
-}
-
-static inline void __s2e_touch_buffer(volatile void *buffer, unsigned size)
-{
-    unsigned i;
-    for (i=0; i<size; ++i) {
-        volatile const char *b = (volatile const char *)buffer;
-        *b;
-    }
-
-}
+#define S2E_INSTRUCTION_SIMPLE(val)                     \
+    S2E_INSTRUCTION_COMPLEX(val, 00)
 
 
 /** Get S2E version or 0 when running without S2E. */
 static inline int s2e_version()
 {
-	int version;
+	int version = 0;
     __asm__ volatile(
-        ".WORD 0xff000000\n\t"   /* S2E opcode to store version in r0 */
+    	S2E_INSTRUCTION_SIMPLE(00)   /* S2E opcode to store version in r0 */
     	".ALIGN\n"
     	"mov %[v], r0\n\t"
         : [v] "+r" (version) /* output */
@@ -77,13 +59,48 @@ static inline int s2e_version()
     return version;
 }
 
+/** Enable symbolic execution. */
+static inline void s2e_enable_symbolic(void)
+{
+    __asm__ __volatile__(
+        S2E_INSTRUCTION_SIMPLE(01)
+        ".ALIGN\n"
+    );
+}
+
+/** Disable symbolic execution. */
+static inline void s2e_disable_symbolic(void)
+{
+    __asm__ __volatile__(
+        S2E_INSTRUCTION_SIMPLE(02)
+        ".ALIGN\n"
+    );
+}
+
+/** Enable forking on symbolic conditions. */
+static inline void s2e_enable_forking(void)
+{
+    __asm__ __volatile__(
+		S2E_INSTRUCTION_SIMPLE(09)
+		".ALIGN\n"
+    );
+}
+
+/** Disable forking on symbolic conditions. */
+static inline void s2e_disable_forking(void)
+{
+    __asm__ __volatile__(
+		S2E_INSTRUCTION_SIMPLE(0A)
+		".ALIGN\n"
+    );
+}
+
 /** Print message to the S2E log. */
 static inline void s2e_message(const char* message)
 {
 	__s2e_touch_string(message);
     __asm__ __volatile__(
-    	"MOV r0, %[msg]\n\t"
-    	".WORD 0xFF100000\n\t"
+    	S2E_INSTRUCTION_SIMPLE(10)
         : /*no output */
     	: [msg] "r" (message)
     	: "r0"
@@ -121,30 +138,12 @@ static inline void s2e_print_expression(const char* name, int expression)
     );
 }
 
-/** Enable forking on symbolic conditions. */
-static inline void s2e_enable_forking(void)
-{
-    __asm__ __volatile__(
-      ".WORD 0xFF090000\n\t"
-      ".ALIGN\n"
-    );
-}
-
-/** Disable forking on symbolic conditions. */
-static inline void s2e_disable_forking(void)
-{
-    __asm__ __volatile__(
-    		".WORD 0xFF0A0000\n\t"
-    	    ".ALIGN\n"
-    );
-}
-
 /** Get the current execution path/state id. */
 static inline unsigned s2e_get_path_id(void)
 {
     unsigned id;
     __asm__ __volatile__(
-    		".WORD 0xFF050000\n\t"
+    		S2E_INSTRUCTION_SIMPLE(05)
     	    ".ALIGN\n"
     		"MOV %[result], r0\n\t"
         : [result] "=r" (id) /* write content to C variable id */
@@ -163,7 +162,25 @@ static inline void s2e_make_symbolic(void* buf, int size, const char* name)
     	"MOV r0, %[buffer]\n\t"
     	"MOV r1, %[symsize]\n\t"
     	"MOV r2, %[symname]\n\t"
-    	".WORD 0xFF030000\n\t"
+    	S2E_INSTRUCTION_SIMPLE(03)
+    	".ALIGN\n\t"
+    	"ldmfd sp!,{r0, r1, r2}\n\t"
+        : /* no output operands */
+        : [buffer] "r" (buf), [symsize] "r" (size), [symname] "r" (name)
+        : "memory", "r0", "r1", "r2"
+    );
+}
+
+static inline void s2e_make_concolic(void *buf, int size, const char *name)
+{
+	__s2e_touch_string(name);
+	__s2e_touch_buffer(buf, size);
+    __asm__ __volatile__(
+    	"stmfd sp!,{r0, r1, r2}\n\t"
+    	"MOV r0, %[buffer]\n\t"
+    	"MOV r1, %[symsize]\n\t"
+    	"MOV r2, %[symname]\n\t"
+    	S2E_INSTRUCTION_SIMPLE(11)
     	".ALIGN\n\t"
     	"ldmfd sp!,{r0, r1, r2}\n\t"
         : /* no output operands */
@@ -180,7 +197,7 @@ static inline void s2e_concretize(void* buf, int size)
         "stmfd sp!,{r0, r1}\n\t"
     	"MOV r0, %[buffer]\n\t"
     	"MOV r1, %[symsize]\n\t"
-        ".WORD 0xFF200000\n\t"
+    	S2E_INSTRUCTION_SIMPLE(20)
         ".ALIGN\n\t"
         "ldmfd sp!,{r0, r1}\n\t"
         : /* no output operands */
@@ -197,7 +214,7 @@ static inline void s2e_get_example(void* buf, int size)
             "stmfd sp!,{r0, r1}\n\t"
         	"MOV r0, %[buffer]\n\t"
         	"MOV r1, %[symsize]\n\t"
-            ".WORD 0xFF210000\n\t"
+    		S2E_INSTRUCTION_SIMPLE(21)
             ".ALIGN\n\t"
             "ldmfd sp!,{r0, r1}\n\t"
             : /* no output operands */
@@ -215,7 +232,7 @@ static inline unsigned s2e_get_example_uint(unsigned val)
             "stmfd sp!,{r1}\n\t"
         	"MOV r0, %[buffer]\n\t"
         	"MOV r1, %[symsize]\n\t"
-            ".WORD 0xFF210000\n\t"
+    		S2E_INSTRUCTION_SIMPLE(21)
             ".ALIGN\n\t"
             "ldmfd sp!,{r1}\n\t"
             : /* no output operands */
@@ -230,11 +247,11 @@ static inline int s2e_get_ram_object_bits()
 {
     int bits;
     __asm__ __volatile__(
-            "stmfd sp!,{r0}\n\t"
-            ".WORD 0xFF520000\n\t"
+            "stmfd sp!,{}\n\t"
+    		S2E_INSTRUCTION_SIMPLE(52)
             ".ALIGN\n\t"
     		"MOV %[robits], r0\n\t"
-            "ldmfd sp!,{r0}\n\t"
+            "ldmfd sp!,{}\n\t"
         : [robits] "=r" (bits)
         : /* no input */
         : "r0"
@@ -251,7 +268,7 @@ static inline void s2e_kill_state(int status, const char* message)
         "stmfd sp!,{r0,r1}\n\t"
     	"MOV r0, %[statcode]\n\t"
     	"MOV r1, %[killmsg]\n\t"
-        ".WORD 0xFF060000\n\t"
+    	S2E_INSTRUCTION_SIMPLE(06)
         ".ALIGN\n\t"
         "ldmfd sp!,{r0,r1}\n\t"
         : /* no output operand*/
@@ -260,17 +277,6 @@ static inline void s2e_kill_state(int status, const char* message)
     );
 }
 
-/* Kills the current state if b is zero */
-static inline void _s2e_assert(int b, const char *expression )
-{
-   if (!b) {
-      s2e_kill_state(0, expression);
-   }
-}
-
-#define s2e_assert(expression) _s2e_assert(expression, "Assertion failed: "  #expression)
-
-
 /** Declare a merge point: S2E will try to merge
  *  all states when they reach this point.
  *
@@ -278,7 +284,7 @@ static inline void _s2e_assert(int b, const char *expression )
 static inline void s2e_merge_point()
 {
     __asm__ __volatile__(
-        ".WORD 0xFF700000\n\t"
+    	S2E_INSTRUCTION_SIMPLE(70)
         ".ALIGN\n\t"
     );
 }
@@ -294,12 +300,198 @@ static inline void s2e_rawmon_loadmodule(const char *name, unsigned loadbase, un
     		"MOV r0, %[rawname]\n\t"
     		"MOV r1, %[rawbase]\n\t"
     		"MOV r2, %[modsize]\n\t"
-            ".WORD 0xFFAA0000\n\t"
+    		S2E_INSTRUCTION_SIMPLE(AA)
             ".ALIGN\n\t"
             "ldmfd sp!,{r0,r1,r2}\n\t"
         : /*no output operand */
     	: [rawname] "r" (name), [rawbase] "r" (loadbase), [modsize] "r" (size)
     	: "r0", "r1", "r2"
+    );
+}
+
+/** Raw monitor plugin */
+/** Communicates to S2E the coordinates of loaded modules. Useful when there is
+    no plugin to automatically parse OS data structures. */
+static inline void s2e_rawmon_loadmodule2(const char *name,
+                                          uint64_t nativebase,
+                                          uint64_t loadbase,
+                                          uint64_t entrypoint,
+                                          uint64_t size,
+                                          unsigned kernelMode)
+{
+    s2e_opcode_module_config_t cfg;
+    cfg.name = (uintptr_t) name;
+    cfg.nativeBase = nativebase;
+    cfg.loadBase = loadbase;
+    cfg.entryPoint = entrypoint;
+    cfg.size = size;
+    cfg.kernelMode = kernelMode;
+
+    __s2e_touch_string(name);
+    __asm__ __volatile__(
+		"stmfd sp!,{r2}\n\t"
+		"MOV r2, %[cfg]\n\t"
+		S2E_INSTRUCTION_COMPLEX(AA, 02)
+		 ".ALIGN\n\t"
+		 "ldmfd sp!,{r2}\n\t"
+        : /* no output operand*/
+        : [cfg] "r" (&cfg)
+      	: "r2"
+    );
+
+}
+
+/** Returns true if ptr points to symbolic memory */
+static inline int s2e_is_symbolic(void *ptr, size_t size)
+{
+    int result;
+    __s2e_touch_buffer(ptr, 1);
+    __asm__ __volatile__(
+		"stmfd sp!,{r0,r2}\n\t"
+		"MOV r0, %[size]\n\t"
+		"MOV r2, %[ptr]\n\t"
+		S2E_INSTRUCTION_SIMPLE(04)
+		".ALIGN\n\t"
+		"MOV %[result], r0\n\t"
+		"ldmfd sp!,{r0,r2}\n\t"
+        : [result] "=r" (result)
+        : [size] "r" (size), [ptr] "r" (ptr)
+      	: "r0", "r2"
+    );
+    return result;
+}
+
+/** Open file from the guest.
+ *
+ * NOTE: This requires the HostFiles plugin. */
+static inline int s2e_open(const char *fname)
+{
+    int fd;
+    __s2e_touch_string(fname);
+    __asm__ __volatile__(
+		"stmfd sp!,{r0,r1,r2}\n\t"
+		"MOV r0, #-1\n\t"
+		"MOV r1, %[fname]\n\t"
+		"MOV r2, #0\n\t"
+		S2E_INSTRUCTION_SIMPLE(EE)
+		"MOV %[fd], r0\n\t"
+		".ALIGN\n\t"
+		"ldmfd sp!,{r0,r1,r2}\n\t"
+        : [fd] "=r" (fd)
+        : [fname] "r" (fname)
+      	: "r0", "r1", "r2"
+    );
+    return fd;
+}
+
+/** Close file from the guest.
+ *
+ * NOTE: This requires the HostFiles plugin. */
+static inline int s2e_close(int fd)
+{
+    int res;
+    __asm__ __volatile__(
+		"stmfd sp!,{r1}\n\t"
+		"MOV r0, #-1\n\t"
+		"MOV r1, %[fd]\n\t"
+    	S2E_INSTRUCTION_COMPLEX(EE, 01)
+		".ALIGN\n\t"
+		"MOV %[res], r0\n\t"
+		"ldmfd sp!,{r1}\n\t"
+        : [res] "=r" (res)
+        : [fd] "r" (fd)
+      	: "r0", "r1"
+    );
+    return res;
+}
+
+/** Read file content from the guest.
+ *
+ * NOTE: This requires the HostFiles plugin. */
+static inline int s2e_read(int fd, char *buf, int count)
+{
+    int res;
+    __s2e_touch_buffer(buf, count);
+    __asm__ __volatile__(
+            "stmfd sp!,{r1,r2,r3}\n\t"
+    		"MOV r0, #-1\n\t"
+    		"MOV r1, %[fd]\n\t"
+    		"MOV r2, %[buf]\n\t"
+    		"MOV r3, %[count]\n\t"
+    		S2E_INSTRUCTION_COMPLEX(EE, 02)
+    		"MOV %[res], r0\n\t"
+            ".ALIGN\n\t"
+            "ldmfd sp!,{r1,r2,r3}\n\t"
+        : [res] "=r" (res)
+        : [fd] "r" (fd), [buf] "r" (buf), [count] "r" (count)
+      	: "r0", "r1", "r2", "r3"
+    );
+    return res;
+}
+
+/** CodeSelector plugin */
+/** Enable forking in the current process (entire address space or user mode only). */
+static inline void s2e_codeselector_enable_address_space(unsigned user_mode_only)
+{
+    __asm__ __volatile__(
+   		"stmfd sp!,{r2}\n\t"
+   		"MOV r2, %[user_mode_only]\n\t"
+    	S2E_INSTRUCTION_SIMPLE(AE)
+		".ALIGN\n\t"
+		"ldmfd sp!,{r2}\n\t"
+        : /* no output operand*/
+        : [user_mode_only] "r" (user_mode_only)
+      	: "r2"
+    );
+}
+
+/** Disable forking in the specified process (represented by its page directory).
+    If pagedir is 0, disable forking in the current process. */
+static inline void s2e_codeselector_disable_address_space(uint64_t pagedir)
+{
+    __asm__ __volatile__(
+		"stmfd sp!,{r2}\n\t"
+		"MOV r2, %[pagedir]\n\t"
+		S2E_INSTRUCTION_COMPLEX(AE, 01)
+		".ALIGN\n\t"
+		"ldmfd sp!,{r2}\n\t"
+		: /* no output operand*/
+		: [pagedir]"r" (pagedir)
+	    : "r2"
+    );
+}
+
+static inline void s2e_codeselector_select_module(const char *moduleId)
+{
+    __s2e_touch_string(moduleId);
+    __asm__ __volatile__(
+		"stmfd sp!,{r2}\n\t"
+		"MOV r2, %[moduleId]\n\t"
+    	S2E_INSTRUCTION_COMPLEX(AE, 02)
+		".ALIGN\n\t"
+		"ldmfd sp!,{r2}\n\t"
+        : /* no output operand*/
+    	: [moduleId] "r" (moduleId)
+      	: "r2"
+    );
+}
+
+/** Programmatically add a new configuration entry to the ModuleExecutionDetector plugin. */
+static inline void s2e_moduleexec_add_module(const char *moduleId, const char *moduleName, int kernelMode)
+{
+    __s2e_touch_string(moduleId);
+    __s2e_touch_string(moduleName);
+    __asm__ __volatile__(
+		"stmfd sp!,{r0,r2,r4}\n\t"
+		"MOV r0, %[moduleName]\n\t"
+		"MOV r2, %[moduleId]\n\t"
+		"MOV r4, %[kernelMode]\n\t"
+		S2E_INSTRUCTION_SIMPLE(AF)
+		 ".ALIGN\n\t"
+		 "ldmfd sp!,{r0,r2,r4}\n\t"
+		: /* no output operand*/
+        : [moduleId] "r" (moduleId), [moduleName] "r" (moduleName), [kernelMode] "r" (kernelMode)
+      	: "r0", "r2", "r4"
     );
 }
 
