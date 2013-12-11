cojocar/s2e

Make use of S2E_TARGET_CONC_LIMIT and CPU_CONC_LIMIT
Minimize target-specific code and number of #ifdef, by reusing the
S2E_TARGET_CONC_LIMIT and CPU_CONC_LIMIT macro when handling CPU state
offsets and areas, in conjuction with the neutral CPUArchState struct.

Signed-off-by: Luca Bruno <lucab@debian.org>

qemu/s2e/MMUFunctionHandlers.cpp
@@ -131,11 +131,7 @@ static ref<Expr> io_read_chk(S2EExecutionState *state,
     std::stringstream ss;
     if (isSymb) {
         //If at least one byte is symbolic, generate a label
-#ifdef TARGET_I386
-        ss << "iommuread_" << hexval(naddr) << "@" << hexval(env->eip);
-#elif TARGET_ARM
-        ss << "iommuread_" << hexval(naddr) << "@" << hexval(env->regs[15]);
-#endif
+        ss << "iommuread_" << hexval(naddr) << "@" << hexval(env->S2E_TARGET_CONC_LIMIT);
     }
 
     //If it is not DMA, then check if it is normal memory

S2E/s2e-old

qemu/s2e/MMUFunctionHandlers.cpp
@@ -131,11 +131,7 @@ static ref<Expr> io_read_chk(S2EExecutionState *state,
     std::stringstream ss;
     if (isSymb) {
         //If at least one byte is symbolic, generate a label
-#ifdef TARGET_I386
-        ss << "iommuread_" << hexval(naddr) << "@" << hexval(env->eip);
-#elif TARGET_ARM
-        ss << "iommuread_" << hexval(naddr) << "@" << hexval(env->regs[15]);
-#endif
+        ss << "iommuread_" << hexval(naddr) << "@" << hexval(env->PROG_COUNTER);
     }
 
     //If it is not DMA, then check if it is normal memory